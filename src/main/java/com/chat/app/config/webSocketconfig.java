d0be3d8847b77c09095f62b533c64de2b970d461
praveensurya2808 <135149674+praveensurya2808@users.noreply.github.com>
2025-01-23 20:05:02 +0530

    chat_start

---
+++ src/main/java/com/chat/app/config/webSocketconfig.java
@@ -0,0 +1,8 @@
+package com.chat.app.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.net.http.WebSocket;
+
+public class webSocketconfig implements WebSocketMessageBrokerConfigurer {
+}
